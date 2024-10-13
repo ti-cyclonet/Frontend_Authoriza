3321c42520153a01091db0fc24507d8b4a79620e
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2024-10-13 15:03:05 -0500

    Componente transversal para la notificacion de alertar con ventanas Toast

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { NotificationsComponent } from './components/notifications/notifications.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    SidebarComponent 
+    SidebarComponent
   ],
   exports: [
     SidebarComponent
