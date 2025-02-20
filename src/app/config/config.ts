ec7847039b4f0937c7bb06b27bf38754d1955e3e
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2025-02-19 21:58:08 -0500

    Inout last version

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,5 @@
+export const BASE_URL = "http://localhost:3000";
+
 export const NAME_APP_LONG = "AUTHORIZA - Access control for CycloNet applications.";
 export const NAME_APP_SHORT = "AUTHORIZA";
 export const DESCRIPTION_APP = "Access control for CycloNet applications.";
