7ab31e5982ccd254f1f5f8613b3e4bd3d4d5d1c5
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2025-06-03 23:19:56 -0500

    login form validations

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,13 +1,110 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { NotificationsComponent } from '../notifications/notifications.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, NotificationsComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  // configuración notificaciones tipo toast
+  toastTitle: string = '';
+  toastType: 'success' | 'warning' | 'danger' | 'primary' = 'success';
+  notifications: Array<{
+    title: string;
+    type: 'success' | 'warning' | 'danger' | 'primary';
+    alertType: 'A' | 'B';
+    container: 0 | 1;
+    visible: boolean;
+  }> = [];
+  SWNTF: number = 0;
+  // ----------------------------------------------
+
+  constructor(
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+    this.notifications = [];
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+
+      this.showToast('Login form completed successfully!', 'success', 'A', 1);
+
+      console.log('Login válido:', { username, password });
+      setTimeout(() => {
+        this.router.navigate(['/home']);
+      }, 1000);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  // Funciones para NOTIFICACIONES
+  addNotification(
+    title: string,
+    type: 'success' | 'warning' | 'danger' | 'primary',
+    alertType: 'A' | 'B',
+    container: 0 | 1
+  ) {
+    this.notifications.push({
+      title,
+      type,
+      alertType,
+      container,
+      visible: true,
+    });
+  }
+
+  removeNotification(index: number) {
+    this.notifications.splice(index, 1);
+  }
+
+  getIconColor() {
+    return 'var(--header-background-color)';
+  }
+
+  showToast(
+    message: string,
+    type: 'success' | 'warning' | 'danger' | 'primary',
+    alertType: 'A' | 'B',
+    container: 0 | 1
+  ) {
+    const notification = {
+      title: message,
+      type,
+      alertType,
+      container,
+      visible: true,
+    };
+    this.notifications.push(notification);
+    this.cdr.detectChanges();
+
+    if (alertType === 'A') {
+      setTimeout(() => {
+        notification.visible = false;
+        this.cdr.detectChanges();
+      }, 5000);
+    }
+  }
+  // ----------------------------------------------
 }
