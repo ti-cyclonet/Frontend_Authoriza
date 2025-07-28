719407d42391ccba1f33634fc512e1021c601da1
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2025-07-28 13:30:39 -0500

    Fix spinner funtionality

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
-import { NotificationsComponent } from './components/notifications/notifications.component';
-
 @NgModule({
   imports: [
     CommonModule,
